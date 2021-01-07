a44a4f109287c748a86deae529f9a54ee68fd9f6
Mikhael Korneev <korneevm@gmail.com>
2021-01-07 21:05:04 +0300

    Learn Python Databases track 1

---
+++ db.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+engine = create_engine('postgres://user:passwd@hoct:5432/db')
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
